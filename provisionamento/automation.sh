c81ccf53fed0de4857b38330e81756c00b98cc48
Yago Almeida <yago.almeida@4linux.com.br>
2021-06-14 20:04:09 -0300

    fix: ajuste no nome do pacote do logstash

--- provisionamento/automation.sh
+++ provisionamento/automation.sh
@@ -2,7 +2,7 @@
     
 #Variaveis
 LOGSTASH_VERSION="7.13.1"
-LOGSTASH_URL="https://artifacts.elastic.co/downloads/logstash/logstash-${LOGSTASH_VERSION}.deb"
+LOGSTASH_URL="https://artifacts.elastic.co/downloads/logstash/logstash-${LOGSTASH_VERSION}-amd64.deb"
 DEPS_PACKAGES="apt-transport-https ca-certificates curl gnupg-agent software-properties-common python3 python3-pip vim openjdk-11-jre wget tree"
 PACKAGES="docker-ce docker-compose"
 PIP_PACKAGES="ComplexHTTPServer ansible"
